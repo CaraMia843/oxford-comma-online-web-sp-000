3a121609ed97b0d611b1778fc1c4be1a1044bfe1
Cara Zellner <carajeanine@gmail.com>
2020-10-10 23:07:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  if 
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2 
+    array[0..1].join(" and ")
+  else array.length == 3
+    array[0...-1].join(", ") <<", and #{array[-1]}"
+  end
 end
